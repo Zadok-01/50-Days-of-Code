faa019b1113defc79bee630ea54ccc9df6ee3f82
Zadok-01 <119855051+Zadok-01@users.noreply.github.com>
2022-12-07 10:10:28 +0000

    Update challenge_02.py

--- challenge_02.py
+++ challenge_02.py
@@ -1,21 +1,17 @@
-'''
-Challenge #2:
-Day 2: Strings to Integers
-Write a function called convert_add that takes a list of strings as an argument and converts it to integers and sums the list. For example [‘1’, ‘3’, ‘5’] should be converted to [1, 3, 5] and
-summed to 9.
-
-Extra Challenge: Duplicate Names
-Write a function called check_duplicates that takes a list of strings as an argument. The function should check if the list has any duplicates. If there are duplicates, the function should return the duplicates. If there are no duplicates, the function should return "No duplicates". For example, the list of fruits below should return apple as a duplicate and list of names should return "no duplicates".
-fruits = ['apple', 'orange', 'banana', 'apple']
-names = ['Yoda', 'Moses', 'Joshua', 'Mark']
-'''
+# Challenge 02
 
 def convert(strings):
     return sum(map(int, strings))
 
+
 example = ['1', '3', '5']
 print(convert(example))
 
+
+
+print('------')
+
+# Challenge 02  --  Extra
 
 def check_duplicates(strings):
     uniq = set(strings)
@@ -33,4 +29,28 @@ names = ['Yoda', 'Moses', 'Joshua', 'Mark']
 print(check_duplicates(fruits))
 print(check_duplicates(fruits2))
 print(check_duplicates(names))
+
+
+
+'''
+Challenge #2:
+
+Day 2: Strings to Integers
+
+Write a function called convert_add that takes a list of strings as an 
+argument and converts it to integers and sums the list. 
+For example [‘1’, ‘3’, ‘5’] should be converted to [1, 3, 5] and summed to 9.
+
+Extra Challenge: Duplicate Names
+
+Write a function called check_duplicates that takes a list of strings as an 
+argument. The function should check if the list has any duplicates. If there 
+are duplicates, the function should return the duplicates. If there are no 
+duplicates, the function should return "No duplicates". For example, 
+the list of fruits below should return apple as a duplicate and list of 
+names should return "no duplicates".
+
+fruits = ['apple', 'orange', 'banana', 'apple']
+names = ['Yoda', 'Moses', 'Joshua', 'Mark']
+'''
 
