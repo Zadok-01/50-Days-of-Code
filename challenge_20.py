0b7d41f8924b172a07fbfd4d381dcd96af616d2e
Zadok-01 <119855051+Zadok-01@users.noreply.github.com>
2022-12-23 10:08:48 +0000

    Add files via upload

---
+++ challenge_20.py
@@ -0,0 +1,48 @@
+# Challenge 20
+
+
+def capitalize(s):
+    return s.title()
+
+
+text = 'i like learning'
+print(capitalize(text))  # 'I Like Learning'
+
+
+
+print('------')
+
+# Challenge 20  --  Extra
+
+
+def reverse_uppercase(s):
+    return [wd[::-1] for wd in s.split() if any(ch.isupper()for ch in wd)]
+
+
+text = 'leArning is hard, bUt if You appLy youRself you can achieVe success'
+print(reverse_uppercase(text))
+# ['gninrAel', 'tUb', 'uoY', 'yLppa', 'flesRuoy', 'eVeihca']
+
+
+
+'''
+Challenge #20:
+
+Day 20: Capitalize First Letter
+
+Write a function called capitalize. This function takes a string as an 
+argument and capitalizes the first letter of each word. For example, 
+‘i like learning’ becomes ‘I Like Learning’.
+
+Extra Challenge: Reversed List
+
+str1 = 'leArning is hard, bUt if You appLy youRself you can achieVe success'
+
+You are given a string of words. Some of the words in the string contain 
+uppercase letters. Write a code that will return all the words that have an 
+uppercase letter. Your code should return a list of the words. Each word in 
+the list should be reversed. Here is how your output should look:
+
+['gninrAel', 'tUb', 'uoY', 'yLppa', 'flesRuoy', 'eVeihca']
+'''
+
