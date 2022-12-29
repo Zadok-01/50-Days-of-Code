48f9844787f199ed1e55d2dedd01c6321a8084e1
Zadok-01 <119855051+Zadok-01@users.noreply.github.com>
2022-12-29 09:58:56 +0000

    Add files via upload

---
+++ challenge_26.py
@@ -0,0 +1,53 @@
+# Challenge 26
+
+
+def sort_words(s):
+    wds = s.split()
+    letters = set(ch for wd in wds for ch in wd)
+    letters = sorted(letters)
+    return [','.join(letters)]
+
+
+txt = 'love life'
+print(sort_words(txt))  # ['e,f,i,l,o,v']
+
+
+
+print('------')
+
+# Challenge 26  --  Extra
+
+
+def string_length(s):
+    return {wd: len(wd) for wd in s.split()}
+
+
+txt = 'Hi my name is Richard'
+print(string_length(txt))
+# {'Hi': 2, 'my': 2, 'name': 4, 'is': 2, 'Richard': 7}
+
+
+
+'''
+Challenge #26:
+
+Day 26: Sort Words
+
+Write a function called sort_words that takes a string of words as an 
+argument, removes the whitespaces, and returns a list of letters sorted 
+in alphabetical order. Letters will be separated by commas. All letters 
+should appear once in the list. This means that you sort and remove 
+duplicates. For example ‘love life’ should return as ['e,f,i,l,o,v'].
+
+Extra Challenge: Length of Words
+
+s = 'Hi my name is Richard'
+
+Write a function called string_length that takes a string of words (words 
+and spaces) as argument. This function should return the length of all the 
+words in the string. Return the results in a form of a dictionary. The 
+string above should return:
+
+{'Hi': 2, 'my': 2, 'name': 4, 'is': 2, 'Richard': 7}
+'''
+
